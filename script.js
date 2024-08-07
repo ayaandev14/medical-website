c54ad66b6b3c1add5eb015a0bc271127fd7e203d
ayaandev14 <ayaanjawaid0@gmail.com>
2024-08-08 01:11:52 +0500

    Add existing project files to Git

---
+++ script.js
@@ -0,0 +1,72 @@
+function open_nav() {
+
+    let opens = document.getElementById("close")
+    opens.classList.remove("close-nav")
+}
+
+
+function nav_close() {
+    let close = document.getElementById("close")
+    close.classList.add("close-nav")
+}
+
+// let icons = document.getElementById("icon");
+// icons.onclick = function () {
+//     let active = document.getElementById("hidden");
+//     active.classList.toggle("hidden");
+//     if (active.classList.contains("hidden")) {
+//         active.style.display = "none";
+//         let box3 = document.getElementById("box-2"); box3.style.backgroundColor = "#fff";
+//         let para3 = document.getElementById("para"); para3.style.color = "black";
+//     } else {
+//         active.style.display = "block";
+//         let box3 = document.getElementById("box-2"); box3.style.backgroundColor = "#00bcd4";
+//         let para3 = document.getElementById("para"); para3.style.color = "#00";
+
+//     }
+// };
+// let icon1 = document.getElementById("icon-1");
+// icon1.onclick = function open1() {
+//     let active1 = document.getElementById("hidden-1");
+//     active1.classList.toggle("hidden-1");
+//     if (active1.classList.contains("hidden-1")) {
+//         active1.style.display = "none";
+//         let box1 = document.getElementById("box-1");
+//         box1.style.backgroundColor = "#fff";
+//         let para1 = document.getElementById("para-1");
+//         para1.style.color = "black"
+
+//     }
+//     else {
+//         active1.style.display = "block";
+//         let box1 = document.getElementById("box-1");
+//         box1.style.backgroundColor = "#00bcd4";
+//         let para = document.getElementById("para-2");
+//         para.style.color = "#000";
+
+
+
+//     }
+// }
+// let icon2 = document.getElementById("icon-2");
+// icon2.onclick = function open1() {
+//     let active2 = document.getElementById("hidden-2");
+//     active2.classList.toggle("hidden-2");
+//     if (active2.classList.contains("hidden-2")) {
+//         active2.style.display = "none";
+//         let box = document.getElementById("box");
+//         box.style.backgroundColor = "#fff";
+//         let para1 = document.getElementById("para-2");
+//         para1.style.color = "black"
+
+//     }
+//     else {
+//         active2.style.display = "block";
+//         let box = document.getElementById("box");
+//         box.style.backgroundColor = "#00bcd4";
+//         let para = document.getElementById("para-2");
+//         para.style.color = "#000";
+
+//     }
+// }
+
